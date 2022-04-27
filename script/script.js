7207b2ce6571a6834417a4a9a1587df50cfe1f30
Enas <enas.elmershati@gmail.com>
2022-04-27 12:36:29 +0200

    Responsive

--- script/script.js
+++ script/script.js
@@ -1,48 +1,14 @@
-// Click on logo, display text
-$('.toggle').click(function() {
-    $(this).next('.div_appear').slideToggle('slow');
-});
+// Show and hide a navbar depending on screen size
+var sidebar = document.getElementById("sidebar");
 
-
-
-// Scroll to appear
-function reveal(item, wrapper) {
-  var verticalEdge   = wrapper.scrollTop() + wrapper.outerHeight(),
-      horizontalEdge = wrapper.scrollLeft() + wrapper.outerWidth();
-  
-  item.each(function() {
-    if ((verticalEdge >= $(this).offset().top) &&
-        (horizontalEdge >= $(this).offset().left)) {
-          $(this).addClass('animate');
-        }
-  });
+function show() {
+  if (sidebar.style.display === 'block') {
+    sidebar.style.display = 'none';
+  } else {
+    sidebar.style.display = 'block';
+  }
 }
-  
-$(window).on("load scroll", function() {
-  reveal($('.reveal'), $(this));
-});
 
-$(window).on("load scroll", function() {
-  reveal($('.reveal_left'), $(this));
-});
-
-$(window).on("load scroll", function() {
-  reveal($('.reveal_right'), $(this));
-});
-
-
-
-
-// Change nav color on different div
-var change_colorOffset = $("#change_color").offset();
-var $contentDivs = $(".content");
-$(document).scroll(function() {
-    $contentDivs.each(function(k) {
-        var _thisOffset = $(this).offset();
-        var _actPosition = _thisOffset.top - $(window).scrollTop();
-        if (_actPosition < change_colorOffset.top && _actPosition + $(this).height() > 0) {
-            $("#change_color").removeClass("light dark").addClass($(this).hasClass("light") ? "light" : "dark");
-            return false;
-        }
-    });
-});
+function hide() {
+  sidebar.style.display = "none";
+}
